2c59006d288718c9506a9e6383baf4ef57a413eb
Elnur21 <elnurme@code.edu.az>
2025-02-16 00:04:11 +0400

    add folder structure for explorer part

--- src/components/Explorer.jsx
+++ src/components/Explorer.jsx
@@ -1,105 +1,70 @@
-import Link from "next/link";
-import Image from "next/image";
 import { useState } from "react";
-import ChevronRight from "../components/icons/ChevronRight";
 import styles from "../styles/Explorer.module.css";
-import { useAppStore } from "@/libs/store";
-import { useRouter } from "next/router";
+import ExplorerItem from "./ExplorerItem";
 
 const explorerItems = [
   {
-    name: "home.jsx",
-    path: "/",
-    icon: "react_icon.svg",
+    title: "General",
+    items: [
+      {
+        name: "home.jsx",
+        path: "/",
+        icon: "react_icon.svg",
+      },
+      {
+        name: "resume.html",
+        path: "/resume",
+        icon: "html_icon.svg",
+      },
+      {
+        name: "contact.yml",
+        path: "/contact",
+        icon: "yml_icon.svg",
+      },
+      {
+        name: "projects.js",
+        path: "/projects-js",
+        icon: "js_icon.svg",
+      },
+      {
+        name: "projects.py",
+        path: "/projects-py",
+        icon: "py_icon.svg",
+      },
+    ],
   },
   {
-    name: "resume.html",
-    path: "/resume",
-    icon: "html_icon.svg",
-  },
-  {
-    name: "contact.yml",
-    path: "/contact",
-    icon: "yml_icon.svg",
-  },
-  {
-    name: "projects.js",
-    path: "/projects-js",
-    icon: "js_icon.svg",
-  },
-  {
-    name: "projects.py",
-    path: "/projects-py",
-    icon: "py_icon.svg",
-  },
-  {
-    name: "languages.json",
-    path: "/languages",
-    icon: "json_icon.svg",
-  },
-  {
-    name: "github.md",
-    path: "/github",
-    icon: "markdown_icon.svg",
+    title: "Github",
+    items: [
+      {
+        name: "languages.json",
+        path: "/languages",
+        icon: "json_icon.svg",
+      },
+      {
+        name: "github.md",
+        path: "/github",
+        icon: "markdown_icon.svg",
+      },
+    ],
   },
 ];
 
 const Explorer = () => {
-  const [portfolioOpen, setPortfolioOpen] = useState(true);
-  const router = useRouter();
-  const addTab = useAppStore((state) => state.addTab);
+  const [activeMenus, setActiveMenu] = useState("General");
 
   return (
     <div className={styles.explorer}>
       <p className={styles.title}>Explorer</p>
-      <div>
-        <input
-          type="checkbox"
-          className={styles.checkbox}
-          id="portfolio-checkbox"
-          checked={portfolioOpen}
-          onChange={() => setPortfolioOpen(!portfolioOpen)}
+      {explorerItems.map((item) => (
+        <ExplorerItem
+          explorerItems={item.items}
+          menuOpen={activeMenus.includes(item.title)}
+          activeMenus={activeMenus}
+          setActiveMenu={setActiveMenu}
+          title={item.title}
         />
-        <label htmlFor="portfolio-checkbox" className={styles.heading}>
-          <ChevronRight
-            className={styles.chevron}
-            style={portfolioOpen ? { transform: "rotate(90deg)" } : {}}
-          />
-          Portfolio
-        </label>
-        <div
-          className={styles.files}
-          style={portfolioOpen ? { display: "block" } : { display: "none" }}
-        >
-          {explorerItems.map((item) => (
-            <Link
-              onClick={() =>
-                addTab({
-                  fileName: item.name,
-                  icon: `/${item?.icon}`,
-                  path: item.path,
-                })
-              }
-              href={item.path}
-              key={item.name}
-            >
-              <div
-                className={`${styles.file}  ${
-                  router.pathname === item.path && styles.active
-                }`}
-              >
-                <Image
-                  src={`/${item.icon}`}
-                  alt={item.name}
-                  height={18}
-                  width={18}
-                />{" "}
-                <p>{item.name}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
